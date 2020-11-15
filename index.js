791172d97d03f4166d4e88bd6368f6e4b667f3ea
Raine Revere <raine@cybersemics.org>
2020-11-15 14:35:59 -0700

    Refactor index.js

--- index.js
+++ index.js
@@ -22,8 +22,6 @@ const argsRegex = /\s*,\s*/
  * - foo && bar
  * - foo:val1
  * - foo:arg1=val1
- *
- * @returns
  */
 function parseExpression(str) {
 
@@ -33,7 +31,7 @@ function parseExpression(str) {
   // extract options from args using filter with side effect
   const options = {}
   const args = compact((expMatches[2] || '').split(argsRegex))
-    .filter(function (arg) {
+    .filter(arg => {
       const keyValue = arg.split('=')
       if (keyValue.length !== 1) {
         options[keyValue[0]] = keyValue[1]
@@ -43,15 +41,13 @@ function parseExpression(str) {
 
   return {
     expression: expMatches[1] || null,
-    args: args,
-    options: options
+    args,
+    options,
   }
 }
 
 /**
  * Parses a string with one or more prefixnote expressions. Extracts and parses each expression.
- *
- * @returns
  */
 function parse(str) {
 
@@ -62,16 +58,13 @@ function parse(str) {
 
   // parse each prefix expression
   const expressions = execAll(allExpressionsRegex, allExpString)
-    // grab the expression content inside the braces
-    .map(function (match) {
-      return match[1]
-    })
-    .map(parseExpression)
+    // grab and parse the expression content inside the braces
+    .map(match => parseExpression(match[1]))
 
   return {
     original: str,
-    value: value,
-    expressions: expressions
+    value,
+    expressions,
   }
 }
 
@@ -86,39 +79,32 @@ function test(prefixnote, data) {
   const parsed = typeof prefixnote === 'string' ? parse(prefixnote) : prefixnote
 
   // no expressions should pass
-  return parsed.original === parsed.value ?
-    { expression: null, args: [], options: {} } :
+  if (parsed.original === parsed.value) {
+    return {
+      expression: null,
+      args: [],
+      options: {}
+    }
+  }
+
+  return find(parsed.expressions, exp => {
+
+    // empty expressions should pass
+    if (!exp.expression) return true
 
     // otherwise return the first expression that passes
-    find(parsed.expressions, function (exp) {
+    const ast = esprima.parse(exp.expression).body[0].expression
+    return staticEval(ast, data)
 
-      // empty expressions should pass
-      if (exp.expression === null) {
-        return true
-      }
-      else {
-        const ast = esprima.parse(exp.expression).body[0].expression
-        return staticEval(ast, data)
-      }
-    }) || null
+  }) || null
 }
 
-// TODO: Convert to async iterable.
 /**
  * Recursively traverses a file tree starting at the given path, treating each filename as a prefixnote. If the prefixnote evalutes to true for the given data, the branch is traversed, otherwise it is ignored.
  *
  * @returns A stream of parsed filenames.
  */
 function parseFiles(dirname, data) {
-
-  // create a readable stream that will be returned
-  // parse objects will be pushed onto the stream as we traverse the file tree
-  const stream = new Readable({ objectMode: true })
-  stream._read = function () {}
-  parseRecursive(dirname).then(function () {
-    stream.push(null) // eslint-disable-line fp/no-mutating-methods
-  })
-  return stream
 
   // the recursive parsing function
   // newDirname is passed to avoid re-parsing the base of the path each traversal
@@ -128,37 +114,45 @@ function parseFiles(dirname, data) {
 
     // get all the files in the directory
     // return a promise so we know when this completes
-    return fs.readdirAsync(dirname).map(function (filename) {
+    return fs.readdirAsync(dirname).map(async filename => {
 
       const filePath = path.join(dirname, filename)
       const parsed = parse(filename)
       const expression = test(parsed, data)
 
-      return expression ?
+      if (!expression) return null
 
-        fs.statAsync(filePath).then(function (stat) {
+      const stat = await fs.statAsync(filePath)
 
-          // generate the destination path
-          const newName = parsed.value
-          const newPath = path.join(newDirname, newName)
+      // generate the destination path
+      const newName = parsed.value
+      const newPath = path.join(newDirname, newName)
 
-          // if it's a file, push the parse object onto the stream
-          if (stat.isFile()) {
-            // eslint-disable-next-line fp/no-mutating-methods
-            stream.push({
-              original: filePath,
-              parsed: newPath,
-              parsedObject: expression
-            })
-          }
-          // if it's a directory, recursively call parseRecursive
-          else {
-            return parseRecursive(filePath, path.join(newDirname, newName), newPath) // RECURSE
-          }
-        }) :
-        null
+      // if it's a file, push the parse object onto the stream
+      if (stat.isFile()) {
+        // eslint-disable-next-line fp/no-mutating-methods
+        stream.push({
+          original: filePath,
+          parsed: newPath,
+          parsedObject: expression
+        })
+      }
+      // if it's a directory, recursively call parseRecursive
+      else {
+        return parseRecursive(filePath, path.join(newDirname, newName), newPath) // RECURSE
+      }
+
     })
   }
+
+  // create a readable stream that will be returned
+  // parse objects will be pushed onto the stream as we traverse the file tree
+  const stream = new Readable({ objectMode: true })
+  stream._read = function () {}
+  parseRecursive(dirname).then(() => {
+    stream.push(null) // eslint-disable-line fp/no-mutating-methods
+  })
+  return stream
 }
 
 module.exports = {
